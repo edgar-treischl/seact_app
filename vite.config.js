ed4e2d173df29b0d64bd847347b1908a83d80a72
edgar.treischl <edgar.treischl@isb.bayern.de>
2025-10-12 08:44:13 +0200

    config fun

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/seact_app/',
-})
+  base: mode === 'production' ? '/seact_app/' : '/',
+}))
 
