b8909e035efd71555ccb127326e59c9b9ff0149b
edgar.treischl <edgar.treischl@isb.bayern.de>
2025-10-13 10:24:21 +0200

    use HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 // src/main.jsx
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import './index.css'; // or wherever your styles are
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </HashRouter>
+  </React.StrictMode>
 );
